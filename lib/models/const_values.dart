1fd11c9817fd23255a5c88a6b4a91f967fc31df4
devtobi16 <tobilobasokoya@gmail.com>
2024-03-20 20:07:22 -0500

    Images

---
+++ lib/models/const_values.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+//Onboarding Images
+const onboardLogo ='assets/images/onboardLogo.png';
+const image1 = 'assets/images/onboard1.png';
+const image2 = 'assets/images/onboard2.png';
+const image3 = 'assets/images/onboard3.png';
+
+//Password Update
+const passwordImage = 'assets/images/passwordUpdate.png';
+//Logo
+const logoImage = 'assets/images/logo.png';
+const logoText = 'assets/images/logoText.png';
+
+const mainColor = Colors.blueAccent;
+const textColor = Color(0xff797979);
+const shadowColor = Color(0xff797979);
+const scaffoldColor = Color(0xffE5E5E5);
+const bigspace = SizedBox(height: 32);
+const smallspace = SizedBox(height: 16);
+const Color navTextColor = Color(0xff111111);
+const Color white = Color(0xffFFFFFF);
+const double textsize = 14;
+const double headersize = 20;
